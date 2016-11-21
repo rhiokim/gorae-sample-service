5f041002d7a2cdd357dcbd7dc10aa9cf6a6f46dc
rhiokim <rhio.kim@gmail.com>
2016-11-21 18:19:47 +0900

    feat: new docker version build script

---
+++ app.v1.js
@@ -0,0 +1,17 @@
+"use strict"
+
+const express = require('express');
+const uuid = require('uuid');
+const os = require('os');
+
+let app = express();
+
+let hostname = os.hostname();
+
+app.get("/guid", (req, res) => {
+    res.json({ "guid": uuid.v4() });
+});
+
+app.listen(9000, () => {
+    console.log("listening on port 9000");
+});
